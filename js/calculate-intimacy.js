51e8bdfecf1c96a42db59585cf966e36cd434cfa
Elmira Ilyasova <capricorn_26@mail.ru>
2025-08-24 21:35:21 +0700

    Добавляет калькулятор близости

---
+++ js/calculate-intimacy.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('form');
+const RangeResult = document.querySelector('#range-result');
+const AverageResult = document.querySelector('#average-result');
+
+const formSubmitHandler = (evt) => {
+  evt.preventDefault();
+  const inputs = evt.target.elements;
+
+  let fix = 0;
+  let min = 0;
+  let max = 0;
+
+  min += Number(inputs['intimicy-bag-3'].value);
+  max += Number(inputs['intimicy-bag-3'].value) * 4;
+  min += Number(inputs['intimicy-bag-2'].value) * 5;
+  max += Number(inputs['intimicy-bag-2'].value) * 8;
+  min += Number(inputs['intimicy-bag-1'].value) * 4;
+  max += Number(inputs['intimicy-bag-1'].value) * 50;
+
+  fix += Number(inputs['intimicy-present-3'].value);
+  fix += Number(inputs['intimicy-present-2'].value) * 2;
+  fix += Number(inputs['intimicy-present-1'].value) * 5;
+
+  fix += Number(inputs['intimicy-coin-4'].value);
+  fix += Number(inputs['intimicy-coin-3'].value) * 2;
+  fix += Number(inputs['intimicy-coin-2'].value) * 5;
+  fix += Number(inputs['intimicy-coin-1'].value) * 25;
+
+  displayResult(min + fix, max + fix);
+  evt.target.reset();
+};
+
+const displayResult = (min, max) => {
+  if (RangeResult && AverageResult) {
+    RangeResult.textContent = `Диапазон близости: ${min} - ${max}`;
+    AverageResult.textContent = `Среднее значение: ${(min + max) / 2}`;
+  }
+};
+
+form.onsubmit = formSubmitHandler;
